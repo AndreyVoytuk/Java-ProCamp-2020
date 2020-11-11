22cffe33f6ad9608994886f92dacacec8bb0f567
A.Voitiuk <Hameleon!23>
2020-11-11 13:41:13 +0200

    init pro camp 2020

---
+++ src/main/java/com/sagax/javaprocamp2020/JavaProcamp2020Application.java
@@ -0,0 +1,13 @@
+package com.sagax.javaprocamp2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProcamp2020Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaProcamp2020Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sagax/javaprocamp2020/JavaProcamp2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagax.javaprocamp2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProcamp2020ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
